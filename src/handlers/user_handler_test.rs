e2f582f0b2894ad09cc96daa87b15695b342096d
jrgood01 <jacob.r.haygood@gmail.com>
2023-05-23 16:06:48 -0400

    added more test :)

--- src/handlers/user_handler_test.rs
+++ src/handlers/user_handler_test.rs
@@ -3,6 +3,7 @@ mod tests {
     use super::*;
     use actix_web::{web, App, Responder, HttpResponse};
     use actix_web::http::StatusCode;
+    use bcrypt::{DEFAULT_COST, hash, verify};
     use sqlx::{Sqlite, SqlitePool};
     use crate::handlers::user_handler::*;
     use crate::models::user::User;
@@ -46,10 +47,55 @@ mod tests {
         assert_eq!(user.username, "test_user");
 
 
-        const verified = verify(String::from("password"), &user.hashed_password).unwrap();
+        let verified = verify(String::from("password"), &user.hashed_password).unwrap();
 
         assert_eq!(verified, true);
     }
+
+    #[actix_rt::test]
+    async fn test_login_valid() {
+        let pool = setup_db().await;
+
+        let new_user = User {
+            id: None,
+            username: String::from("test_user"),
+            password: String::from("password"),
+        };
+
+        let result = add_user(web::Data::new(pool.clone()), web::Json(new_user)).await;
+
+        assert_eq!(result.status(), StatusCode::OK);
+
+        let login_result = login(web::Data::new(pool.clone()), web::Json(Credentials {
+            username: String::from("test_user"),
+            password: String::from("password"),
+        })).await;
+
+        assert_eq!(login_result.status(), StatusCode::OK); 
+    }
+
+    #[actix_rt::test]
+    async fn test_login_invalid() {
+        let pool = setup_db().await;
+
+        let new_user = User {
+            id: None,
+            username: String::from("test_user"),
+            password: String::from("password"),
+        };
+
+        let result = add_user(web::Data::new(pool.clone()), web::Json(new_user)).await;
+
+        assert_eq!(result.status(), StatusCode::OK);
+
+        let login_result = login(web::Data::new(pool.clone()), web::Json(Credentials {
+            username: String::from("test_user"),
+            password: String::from("incorrect_password"),
+        })).await;
+
+        assert_eq!(login_result.status(), StatusCode::UNAUTHORIZED); 
+    }
+
 
     /*
     #[actix_rt::test]
